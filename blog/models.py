09407876fc41b148951fd68f85eb25d517b9502a
Jeremy Scott <jeremycscott@gmail.com>
2019-11-08 16:38:37 -0500

    added a home page

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.TimeField()
-    body = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/')
