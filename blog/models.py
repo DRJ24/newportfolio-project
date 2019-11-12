87dda221e36f95c1c4c02e893e64472adad6a8d3
Jeremy Scott <jeremycscott@gmail.com>
2019-11-12 17:44:29 -0500

    typo

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-import datetime
+from datetime import datetime
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(default=datetime.now, blank=True)
+    pub_date = models.DateTimeField(default=datetime.now(), blank=True)
     body = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/')
