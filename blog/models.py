bda494f40386e55a5cdcf03a97cec2244edd94f7
Jeremy Scott <jeremycscott@gmail.com>
2019-11-12 17:38:44 -0500

    add default datetime

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/')
