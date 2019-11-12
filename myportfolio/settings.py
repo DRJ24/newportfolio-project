5b4bfc996857cb63e362ca62eb5090b41960879a
Jeremy Scott <jeremycscott@gmail.com>
2019-11-12 16:45:51 -0500

    added requirements

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'django123',
+        'PASSWORD': 'Sandy123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
